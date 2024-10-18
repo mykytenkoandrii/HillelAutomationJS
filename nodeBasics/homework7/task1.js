48558e1c9c61f60a0ae93053319bbb72df9e5974
andriimyk <Andrii.Mykytenko@TX-N-0780>
2024-10-18 23:57:00 +0300

    homework 7

---
+++ nodeBasics/homework7/task1.js
@@ -0,0 +1,18 @@
+// Створіть функцію handleNum яка буде приймати 3 параметри.
+// число
+// Колбек функцію яку треба викликати якщо передане число парне
+// Колбек функцію яку треба викликати якщо передане число непарне
+// Створіть ще дві функції які ви будете передавати у якості колбеків, наприклад handleEven та handleOdd. 
+//Кожна з них має виводити просте повідомлення в консоль. 
+//Наприклад handleEven буде виводити текст “number is even”, a handleOdd буде виводити текст “number is odd”
+// Викличте функцію handleNum і передайте в якості аргументів довільне число і дві функції які ви створили раніше
+let handleEven = (number) => { console.log(`${number} is even`) };
+let handleOdd = (number) => { console.log(`${number} is odd`) };
+
+function handleNum(num, evenFunc, oddFunc){
+    let result = num % 2 === 0 ? true : false;
+    result == true ? evenFunc(num) : oddFunc(num);
+}
+
+handleNum(2, handleEven, handleOdd);
+handleNum(3, handleEven, handleOdd);
---
+++ nodeBasics/homework7/task3.js
@@ -0,0 +1,34 @@
+// Створіть функцію divide, яка приймає два параметри: numerator і denominator та повертає як результат виконання число отримане від ділення.
+// У функції треба поділити numerator на denominator і повернути результат.
+// Додайте валідацію в функції. У разі,
+// якщо denominator дорівнює 0
+// або хоча б один з аргументів не є числом, викиньте помилку з інформативним повідомленням,
+// Викличте функцію divide з різними значеннями numerator і denominator, включаючи випадок, коли denominator дорівнює 0 або один з аргументів не є числом.
+// Огорніть кожен окремий виклик функції divide в try…catch.Використовуючи блок finally, виведіть повідомлення "Робота завершена" в консоль, навіть якщо помилка виникла або не виникла.
+
+function divide(numerator, denominator){
+    if(denominator === 0){
+        throw new Error(`Prohibited action: division by zero`);
+    }
+    else if(typeof(numerator) !== 'number' || typeof(denominator) !== 'number'){
+        throw new Error(`Incorrect type of argument: type of numerator - ${typeof(numerator)}, type of denominator - ${typeof(denominator)}`);
+    }
+    return numerator / denominator;
+}
+
+function runFunction(inputFunction, arg1, arg2){
+    try{
+        return inputFunction(arg1, arg2);
+    } catch(Error){
+        console.log(Error.message);
+        return 0;
+    } finally{
+        console.log(`Function '${inputFunction.name}' has been finished`);
+    }
+}
+
+console.log(runFunction(divide, 10, 2));
+console.log(runFunction(divide, 1, 2));
+console.log(runFunction(divide, 2, 0));
+console.log(runFunction(divide, '2', 2));
+console.log(runFunction(divide, 2, 'x'));
