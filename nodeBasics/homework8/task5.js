c762871757477a6462f95e791434519ce5860286
andriimyk <Andrii.Mykytenko@TX-N-0780>
2024-10-23 18:18:03 +0300

    homework 8

---
+++ nodeBasics/homework8/task5.js
@@ -0,0 +1,13 @@
+// Вам потрібно створити програму, яка об'єднає два масиви в один
+
+// Ось кроки, які вам потрібно виконати:
+
+// Створіть два масиви, наприклад, firstArray і secondArray, які містять деякі значення.
+// Створіть новий масив, який містить всі елементи з обох вихідних масивів.
+// Виведіть отриманий об'єднаний масив на консоль.
+let firstAarray = ['apple', 'banana', 'orange'];
+let secondAarray = ['strawberry', 'kiwi', 'grape', 'pineapple'];
+
+let concatedArray = firstAarray.concat(secondAarray);
+
+console.log(concatedArray);
