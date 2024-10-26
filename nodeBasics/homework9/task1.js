937d26d176ada0ef3ad4ee9535dd870ac9a48b67
andriimyk <Andrii.Mykytenko@TX-N-0780>
2024-10-26 16:43:07 +0300

    homework 9

---
+++ nodeBasics/homework9/task1.js
@@ -0,0 +1,15 @@
+// Створіть порожній об'єкт з ім'ям student.
+// Додайте до об'єкта student наступні властивості:
+// firstName: рядок, представляє ім'я студента.
+// lastName: рядок, представляє прізвище студента.
+// age: число, представляє вік студента.
+// courses: масив, представляє список курсів, які він вивчає (наприклад, ["Математика", "Історія", "Програмування"]).
+// Виведіть об'єкт student в консоль.
+
+const student = {};
+student.firstName = "Taras";
+student.lastName = "Shevchenko";
+student.age = 13;
+student.courses = ["Mathematics", "Graphics", "Ukrainian language"];
+
+console.log(student);
---
+++ nodeBasics/homework9/task4.js
@@ -0,0 +1,19 @@
+// Створіть об'єкт person з такими властивостями:
+// firstName: рядок, представляє ім'я особи.
+// lastName: рядок, представляє прізвище особи.
+// age: число, представляє вік особи.
+// Додайте до об'єкта person властивість email, яка буде містити електронну пошту особи.
+// Видаліть властивість age з об'єкта person.
+// Виведіть оновлений об'єкт person в консоль.
+
+const person = {
+    firstName: "Joe",
+    lastName: "Biden",
+    age: 81
+}
+
+person.email = "president@whitehouse.gov";
+
+delete person.age;
+
+console.log(person);
