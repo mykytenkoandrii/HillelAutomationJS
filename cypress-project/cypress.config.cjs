9f9c73f2bd0c2e2d3ed7aa0981147c698ba8bfa3
andriimyk <Andrii.Mykytenko@TX-N-0780>
2024-12-10 20:28:05 +0200

    homework 17

--- cypress-project/cypress.config.cjs
+++ cypress-project/cypress.config.cjs
@@ -2,8 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+      baseUrl: "https://example.cypress.io",
+
+      retries: {
+        runMode: 2,
+        openMode: 2
+      },
+
+      defaultCommandTimeout: 10000,
+      viewportHeight: 1080,
+      viewportWidth: 1920,
+      screenshotsFolder: "cypress/screenshots",
+      screenshotOnRunFailure: true,
+      video: true,
+      videosFolder: "cypress/videos"
     },
-  },
-});
+  });
